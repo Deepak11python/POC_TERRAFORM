c98711ae48f5fb7a65e282a7ad7427de2f8813b8
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2025-05-07 15:04:07 +0530

    Update variable.tf

--- GCE/variable.tf
+++ GCE/variable.tf
@@ -1,19 +1,19 @@
 # Define variables used in the main configuration
-
 # GCP project ID
-
 variable "p_id" {
   description = "This is a GCP Project id"
   type        = string
   default     = "helpful-metric-459104-j4"
 }
 
+# GCP GCE Machine Type
 variable "machine_type" {
   description = "This is the type of the machine"
   type        = string
   default     = "e2-micro"
 }
 
+# GCP Zone
 variable "gcp_compute_zone" {
   description = "This is the zone in which the compute instances will be created"
   type        = string
