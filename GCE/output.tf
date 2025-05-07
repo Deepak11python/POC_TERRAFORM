d5e4734c757195621094f2e35fb9e519f28f1c20
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2025-05-07 13:25:00 +0530

    Create output.tf

---
+++ GCE/output.tf
@@ -0,0 +1,12 @@
+output "p_project_location" {
+        value = "us-central1-c"
+}
+
+output  "machine_type" {
+        value = "e2-micro"
+}
+
+output  "gcp_compute_zone" {
+
+        value = "us-central1-c"
+}
