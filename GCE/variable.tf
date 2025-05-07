df43014d12826fa3e6349cf2fc87d764f7571654
Deepak11python <49120748+Deepak11python@users.noreply.github.com>
2025-05-07 13:23:03 +0530

    Create variable.tf

---
+++ GCE/variable.tf
@@ -0,0 +1,34 @@
+# Define variables used in the main configuration
+
+# GCP project ID
+
+variable "p_project_location" {
+  description = "This is a GCP Project id"
+  type        = string
+  default     = "helpful-metric-459104-j4"
+}
+
+variable "machine_type" {
+  description = "This is the type of the machine"
+  type        = string
+}
+
+variable "gcp_compute_zone" {
+  description = "This is the zone in which the compute instances will be created"
+  type        = string
+  default     =  "us-central1-a"
+}
+
+# Region for the provider
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+# Zone to deploy the instance
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
